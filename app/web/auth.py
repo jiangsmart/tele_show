513d2c73e7b6e6e30b5b3293f28059952236c2b8
Jiang Xilun <jiangxilun@foxmail.com>
2019-03-24 22:17:24 +0800

    small changes

--- app/web/auth.py
+++ app/web/auth.py
@@ -1,11 +1,45 @@
-from flask import render_template
+from flask import render_template, request, url_for, redirect, current_app, flash
+from flask_login import login_user, login_required
 from . import web
+from app.models.user import User
+import sys
+
+sys.path.append('../neo4j/')
+from conversation import Graph4Match
+
+
+@web.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        graph = Graph4Match(current_app.config['NEO4J_IP'], current_app.config['NEO4J_USER'],
+                            current_app.config['NEO4J_PASSWORD'])
+        login_user(user, False)
+        return redirect(url_for('web.home'))
+    return render_template('auth/register.html')
+    # form = RegisterForm(request.form)
+    # if request.method == 'POST' and form.validate():
+    #     user = User()
+    #     user.set_attrs(form.data)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     login_user(user, False)
+    #     return redirect(url_for('web.index'))
+    # return render_template('auth/register.html', form=form)
 
 
 @web.route('/login', methods=['GET', 'POST'])
 def login():
+    # form = LoginForm(request.form)
+    if request.method == 'POST':
+        graph = Graph4Match(current_app.config['NEO4J_IP'], current_app.config['NEO4J_USER'],
+                            current_app.config['NEO4J_PASSWORD'])
+        user = User().first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=True)
+            next = request.args.get('next')
+            if not next or not next.startswith('/'):
+                next = url_for('web.index')
+            return redirect(next)
+        else:
+            flash('账号不存在或密码错误', category='login_error')
     return render_template('auth/login.html')
-
-@web.route('/register', methods=['GET', 'POST'])
-def register():
-    return render_template('auth/register.html')
--- app/web/mapserve.py
+++ app/web/mapserve.py
@@ -4,4 +4,21 @@ from . import web
 
 @web.route('/mapserve', methods=['GET'])
 def mapserve():
-    return render_template('map/mapserve.html')
+    points = list()
+    with open('app/web/scene_show.csv', 'r') as fin:
+        for line in fin:
+            id, (lon, lat, r) = line.strip().split(',')[0], map(float, line.strip().split(',')[1:])
+            points.append((id, lon, lat, r))
+    # print(points)
+    return render_template('map/mapserve.html', points=points)
+
+
+@web.route('/mapserve1', methods=['GET'])
+def mapserve1():
+    points = list()
+    with open('app/web/station.csv', 'r') as fin:
+        for line in fin:
+            id, (lon, lat, r) = line.strip().split(',')[0], map(float, line.strip().split(',')[1:])
+            points.append((id, lon, lat, r))
+    # print(points)
+    return render_template('map/mapserve1.html', points=points)
